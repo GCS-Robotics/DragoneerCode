dca3978f2bf5ba0acb33b0e7306ec229392f2d24
snakeAttak <horjusotto@gmail.com>
2025-09-15 17:10:35 -0400

    Test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ColorSensorTest.java
@@ -1,4 +1,13 @@
 package org.firstinspires.ftc.teamcode;
 
-public class ColorSensorTest {
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "Color Sensing")
+public class ColorSensorTest extends LinearOpMode {
+    public void runOpMode() throws InterruptedException {
+        float red;
+        float green;
+        float blue;
+    }
 }

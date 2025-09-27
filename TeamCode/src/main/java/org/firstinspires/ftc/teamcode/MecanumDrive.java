18e690210468a3c819ba12cbc45560fe12b3d239
Hamzah M <hamzahm@glenelg.org>
2025-09-27 14:21:51 -0400

    i commented stuff out because there were errors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -219,11 +219,11 @@ public final class MecanumDrive {
     }
 
     public MecanumDrive(HardwareMap hardwareMap, Pose2d pose) {
-        LynxFirmware.throwIfModulesAreOutdated(hardwareMap);
+        //LynxFirmware.throwIfModulesAreOutdated(hardwareMap);
 
-        for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
-            module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
-        }
+        //for (LynxModule module : hardwareMap.getAll(LynxModule.class)) {
+        //    module.setBulkCachingMode(LynxModule.BulkCachingMode.AUTO);
+        //}
 
         // TODO: make sure your config has motors with these names (or change them)
         //   see https://ftc-docs.firstinspires.org/en/latest/hardware_and_software_configuration/configuring/index.html

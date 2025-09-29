b65de8a4c760e201b88077d85e0e6e013676f24f
david <davidc1@glenelg.org>
2025-09-29 16:20:46 -0400

    Idea for TeleOp controls in conjunction with RoadRunner (Could work. could not.) - Removed some imports

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/PathFollowing.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.acmerobotics.roadrunner.PoseVelocity2d;
+import com.acmerobotics.roadrunner.Vector2d;
+
+
+@Autonomous(name="Path Following")
+public class PathFollowing extends LinearOpMode {
+
+    MecanumDrive runner;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        runner = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        waitForStart();
+        while(opModeIsActive()){
+            Vector2d direction = new Vector2d(10,0);
+            runner.setDrivePowers(new PoseVelocity2d(direction, 0));
+            wait(50);
+        }
+    }
+}
+

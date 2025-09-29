ef186ee17f9f928caee24e5fc0365e173a2e24b8
Robert <robertakurc@gmail.com>
2025-09-29 17:06:53 -0400

    Working TeleOp Drive!!!

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerTeleOp.java
@@ -1,17 +1,26 @@
 package org.firstinspires.ftc.teamcode;
 
+import static java.lang.Math.abs;
+
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.PoseVelocity2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
+@TeleOp(name = "TeleOp with Road Runner")
 public final class RoadRunnerTeleOp extends LinearOpMode {
-    MecanumDrive runner = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+    MecanumDrive runner;
 
     @Override
     public void runOpMode() throws InterruptedException {
-        Vector2d direction = new Vector2d(gamepad1.left_stick_x, gamepad1.left_stick_y);
-        runner.setDrivePowers(new PoseVelocity2d(direction, 0));
+        runner = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        waitForStart();
+        while(opModeIsActive()) {
+            Vector2d direction = new Vector2d(-gamepad1.left_stick_y, -gamepad1.left_stick_x);
+            double angle = -90 * gamepad1.right_stick_x;
+            runner.setDrivePowers(new PoseVelocity2d(direction, angle));
+        }
     }
 }

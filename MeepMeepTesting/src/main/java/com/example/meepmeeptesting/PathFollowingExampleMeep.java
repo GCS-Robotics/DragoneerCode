b3a1f169d89369c0452abd573ab89e0c84b82019
david <davidc1@glenelg.org>
2025-10-04 15:11:58 -0400

    Concept path following program with Roadrunner.

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/PathFollowingExampleMeep.java
@@ -0,0 +1,31 @@
+package com.example.meepmeeptesting;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class PathFollowingExampleMeep {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(1000);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngleVel, maxAngAccel, track width
+                .setConstraints(25, 30, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(0)))
+                .setReversed(true)
+                .splineToLinearHeading(new Pose2d(5, -35, Math.toRadians(270)), Math.toRadians(90))
+                .build());
+
+        myBot.setDimensions(15, 10);
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_JUICE_BLACK)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}

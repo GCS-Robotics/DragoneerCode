b82f2d058f652709deb5b754cb9624116b7166ec
Robert <robertakurc@gmail.com>
2025-10-22 16:52:46 -0400

    Cleaned up drive code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DecodeDrive.java
@@ -4,90 +4,88 @@ import static java.lang.Math.abs;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Gamepad;
 
 public class DecodeDrive extends LinearOpMode {
-    // Define any variables here!
-    // Also make variables for hardware, but don't set it yet.
-    double speed = 1.0;
-    // Drive motors
+    // Variables for Stuff
+    double SPEED = 1.0;
+    double DEADZONE = 0.4;
+    // Drive Motors
     DcMotor leftFront;
     DcMotor rightFront;
     DcMotor leftRear;
     DcMotor rightRear;
-    // Launcher motors
+    // Launcher Motors
     DcMotor launcherRight;
     DcMotor launcherLeft;
-    // Intake motor
+    // Intake Motor
     DcMotor intake;
-    DcMotor[] driveMotors = {leftFront, rightFront, leftRear, rightRear};
+    // Extra Tools
+    DcMotor[] allMotors = {leftFront, rightFront, leftRear, rightRear, launcherRight, launcherLeft};
     MecanumDrive drive = new MecanumDrive(leftFront, rightFront, leftRear, rightRear, 1, false, false, false, false);
+
     @Override
     public void runOpMode() throws InterruptedException {
-        // Do hardware map here!
-
-        leftFront=hardwareMap.dcMotor.get("leftFront");
-        rightFront=hardwareMap.dcMotor.get("rightFront");
-        leftRear=hardwareMap.dcMotor.get("leftRear");
-        rightRear=hardwareMap.dcMotor.get("rightRear");
-        launcherRight=hardwareMap.dcMotor.get("launcherRight");
-        launcherLeft=hardwareMap.dcMotor.get("launcherLeft");
-        for(DcMotor motor : driveMotors){
+        // Hardware Map
+        leftFront = hardwareMap.dcMotor.get("leftFront");
+        rightFront = hardwareMap.dcMotor.get("rightFront");
+        leftRear = hardwareMap.dcMotor.get("leftRear");
+        rightRear = hardwareMap.dcMotor.get("rightRear");
+        launcherRight = hardwareMap.dcMotor.get("launcherRight");
+        launcherLeft = hardwareMap.dcMotor.get("launcherLeft");
+        // Setting Motor Stuff
+        for (DcMotor motor : allMotors) {
             motor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
             motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
             motor.setPower(0);
         }
         waitForStart();
-        while(opModeIsActive()){
-            // Do driving action here!
-            speed = (1 - (gamepad1.right_trigger/1.4));
-            if (speed <= 0.1) {
-                speed = 0.1;
+        // The Actual OpMode
+        while (opModeIsActive()) {
+            /// Gamepad 1
+            // Variable Speed Control
+            SPEED = (1 - (gamepad1.right_trigger / 1.4));
+            if (SPEED <= 0.1) {
+                SPEED = 0.1;
             }
-            }
-            if (abs(gamepad1.left_stick_y)+abs(gamepad1.left_stick_x) > 0.4) {
-                // ANYTHING with left stick
+            // Left-Stick Movement
+            if (abs(gamepad1.left_stick_y) + abs(gamepad1.left_stick_x) > DEADZONE) {
                 if (gamepad1.left_stick_y > 0) {
-                    drive.moveForward(speed);
+                    drive.moveForward(SPEED);
 
                 } else if (gamepad1.left_stick_y < 0) {
-                drive.moveBackward(speed);
-                }else if(gamepad1.left_stick_x > 0) {
-                    drive.moveRight(speed);
-                }else if(gamepad1.left_stick_x < 0) {
-                    drive.moveLeft(speed);
+                    drive.moveBackward(SPEED);
+                } else if (gamepad1.left_stick_x > 0) {
+                    drive.moveRight(SPEED);
+                } else if (gamepad1.left_stick_x < 0) {
+                    drive.moveLeft(SPEED);
                 }
             }
-            else if (abs(gamepad1.right_stick_y)+abs(gamepad1.right_stick_y) > 0.4){
-                // ANYTHING with right stick
+            // Right-Stick Movement
+            else if (abs(gamepad1.right_stick_y) + abs(gamepad1.right_stick_y) > DEADZONE) {
                 if (gamepad1.right_stick_x > 0) {
-                    drive.turnRightTank(speed);
-
-                }else if(gamepad1.right_stick_x < 0) {
-                    drive.turnLeftTank(speed);
+                    drive.turnRightTank(SPEED);
+                } else if (gamepad1.right_stick_x < 0) {
+                    drive.turnLeftTank(SPEED);
                 }
-            }
-            else {
+            // No Movement
+            } else {
                 drive.stop();
             }
-
+            /// Gamepad 2
             // Launcher
-            if (gamepad2.a){
+            if (gamepad2.a) {
                 launcherRight.setPower(1);
                 launcherLeft.setPower(1);
-            }
-            else{
+            } else {
                 launcherRight.setPower(0);
                 launcherLeft.setPower(0);
             }
-
             // Intake
-            if (gamepad2.b){
+            if (gamepad2.b) {
                 intake.setPower(0.5);
-            }
-            else{
+            } else {
                 intake.setPower(0);
             }
         }
     }
-
+}

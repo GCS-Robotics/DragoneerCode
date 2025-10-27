98ecfd1c48f48d3da0465d95532b7bdfa11bc471
david <davidc1@glenelg.org>
2025-10-27 16:46:32 -0400

    Steve's test to see if it works

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteveChallenge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteveChallenge.java
@@ -1,29 +1,25 @@
 package org.firstinspires.ftc.teamcode;
 
-
-import static java.lang.Math.PI;
-
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.Servo;
 
 @Autonomous(name = "SteveChallenge")
 public class SteveChallenge extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
+        Servo servo = hardwareMap.servo.get("servo");
+
         waitForStart();
-        int count = 0;
-        while(opModeIsActive()) {
-            telemetry.addData("COUNTING:","%d",count);
-            count++;
-            sleep(1000);
-        }
-        if (isStopRequested()) return;
+
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0, 0, 0))
                         .lineToX(64)
-                        .build());
+                        .lineToX(0)
+                        .build()
+        );
     }
 }

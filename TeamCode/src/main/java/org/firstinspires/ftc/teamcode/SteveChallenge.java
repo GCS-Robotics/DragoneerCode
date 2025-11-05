0a2bf8079b716a6b5044e6edc5f235a12a1e02eb
Hamzah M <hamzahm@glenelg.org>
2025-11-05 17:41:42 -0500

    Prepared code for game time.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteveChallenge.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SteveChallenge.java
@@ -23,7 +23,7 @@ public class SteveChallenge extends LinearOpMode {
 
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(0, 0, 0))
-                        .turn(PI * 2, new TurnConstraints(1, 1, 1))
+                        .turn(PI * 2, new TurnConstraints(0.1, 0, 0.05))
                        // .lineToX(10)
                        // .lineToX(0)
                       //  .turn(PI / 2)

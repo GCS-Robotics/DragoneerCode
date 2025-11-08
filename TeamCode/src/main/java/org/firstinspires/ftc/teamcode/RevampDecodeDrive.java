6fd2a060206f461866bd549f41a734a0fd41a54e
david <davidc1@glenelg.org>
2025-11-08 13:41:04 -0500

    Did a thing pt. 2

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevampDecodeDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RevampDecodeDrive.java
@@ -2,9 +2,11 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DistanceSensor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.hardware.UltrasonicSensor;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
@@ -15,6 +17,7 @@ public class RevampDecodeDrive {
     double drumSpeed = 0.8;
     double DEADZONE;
     final int ROTATION_TICK = 537;
+    int[] balls = new int[3]; // Ball at balls[1] is the ball that should be primed to fire
     DcMotor leftFront;
     DcMotor rightFront;
     DcMotor leftRear;
@@ -32,23 +35,26 @@ public class RevampDecodeDrive {
 
     /**
      * Constructs a master decode drive.
+     *
      * @param hardwareMap Finds all of the hardware components from the Hardware Map
-     * @param tel For any functions that want to post to telemetry (must call telemetry.update() separately)
+     * @param tel         For any functions that want to post to telemetry (must call telemetry.update() separately)
      */
-    public RevampDecodeDrive(HardwareMap hardwareMap, Telemetry tel){
+    public RevampDecodeDrive(HardwareMap hardwareMap, Telemetry tel) {
         this(hardwareMap, tel, 1.0, 1.0, 1.0, 0.8, 0.2);
     }
+
     /**
      * Constructs a master decode drive.
+     *
      * @param hardwareMap Finds all of the hardware components from the Hardware Map
-     * @param tel For any functions that want to post to telemetry (must call telemetry.update() separately)
-     * @param s Drive Speed
-     * @param intakeS Intake Speed
-     * @param launchS Launch Speed
-     * @param drumS Drum Speed
-     * @param dz Deadzone for driving inputs
+     * @param tel         For any functions that want to post to telemetry (must call telemetry.update() separately)
+     * @param s           Drive Speed
+     * @param intakeS     Intake Speed
+     * @param launchS     Launch Speed
+     * @param drumS       Drum Speed
+     * @param dz          Deadzone for driving inputs
      */
-    public RevampDecodeDrive(HardwareMap hardwareMap, Telemetry tel, double s, double intakeS, double launchS, double drumS, double dz){
+    public RevampDecodeDrive(HardwareMap hardwareMap, Telemetry tel, double s, double intakeS, double launchS, double drumS, double dz) {
         telemetry = tel;
         leftFront = hardwareMap.dcMotor.get("leftFront");
         rightFront = hardwareMap.dcMotor.get("rightFront");
@@ -63,7 +69,7 @@ public class RevampDecodeDrive {
         kicker.setPosition(1);
         drive = new MecanumDrive(leftFront, rightFront, leftRear, rightRear, s, true, false, false, false);
         DcMotor[] motors = {leftFront, rightFront, leftRear, rightRear, launcherLeft, launcherRight, drumRotor};
-        for(DcMotor motor : motors){
+        for (DcMotor motor : motors) {
             motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         }
         drumRotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -76,106 +82,118 @@ public class RevampDecodeDrive {
         drumSpeed = drumS;
         DEADZONE = dz;
     }
-    private int reverse(boolean do_it){
-        if(do_it) return -1;
+
+    private int reverse(boolean do_it) {
+        if (do_it) return -1;
         return 1;
     }
+
     /**
      * Set the launch speed
+     *
      * @param newSpeed Between 0 and 1
      */
-    public void setLaunchSpeed(double newSpeed){
+    public void setLaunchSpeed(double newSpeed) {
         launchSpeed = newSpeed;
     }
 
     /**
      * Get the launch speed
+     *
      * @return Launch Speed
      */
-    public double getLaunchSpeed(){
+    public double getLaunchSpeed() {
         return launchSpeed;
     }
 
     /**
      * Set the drum speed
+     *
      * @param newSpeed Between 0 and 1
      */
-    public void setDrumSpeed(double newSpeed){
+    public void setDrumSpeed(double newSpeed) {
         drumSpeed = newSpeed;
     }
 
     /**
      * Get the drum speed.
+     *
      * @return Drum Speed
      */
-    public double getDrumSpeed(){
+    public double getDrumSpeed() {
         return drumSpeed;
     }
 
     /**
      * Set the intake speed
+     *
      * @param newSpeed Between 0 and 1
      */
-    public void setIntakeSpeed(double newSpeed){
+    public void setIntakeSpeed(double newSpeed) {
         intakeSpeed = newSpeed;
     }
 
     /**
      * Get the intake speed
+     *
      * @return Intake Speed
      */
-    public double getIntakeSpeed(){
+    public double getIntakeSpeed() {
         return intakeSpeed;
     }
 
     /**
      * Get the deadzone value.
+     *
      * @return Deadzone
      */
-    public double getDeadzone(){
+    public double getDeadzone() {
         return DEADZONE;
     }
+
+
     /**
      * Runs all of the drive commands using the left and right sticks of a gamepad.
+     *
      * @param gamepad1 Which gamepad's left and right sticks?
      */
-    public void runDrive(Gamepad gamepad1){
+    public void runDrive(Gamepad gamepad1) {
         // QOL #1: Set the Speed
-        double speed = 1-(gamepad1.right_trigger/1.4);
-        if(speed<=0.1){
-            speed=.1;
+        double speed = 1 - (gamepad1.right_trigger / 1.4);
+        if (speed <= 0.1) {
+            speed = .1;
         }
         // QOL #2: Reverse Controls
-        if(gamepad1.left_trigger>.3){
-            speed=speed*(-1);
+        if (gamepad1.left_trigger > .3) {
+            speed = speed * (-1);
         }
         drive.setDriveSpeed(speed);
-        if (Math.abs(gamepad1.right_stick_x) >.4) { // If the right stick is being moved sufficiently
-            if(speed<0){
-                speed=Math.abs(speed);
+        if (Math.abs(gamepad1.right_stick_x) > .4) { // If the right stick is being moved sufficiently
+            if (speed < 0) {
+                speed = Math.abs(speed);
                 drive.setDriveSpeed(speed);
             }
             // Tank Turn
-            if(gamepad1.right_stick_x>.4) {
-                drive.turnRightTank(1*gamepad1.right_stick_x);
+            if (gamepad1.right_stick_x > .4) {
+                drive.turnRightTank(1 * gamepad1.right_stick_x);
             }
-            if(gamepad1.right_stick_x<-.4) {
-                drive.turnLeftTank(1*-gamepad1.right_stick_x);
+            if (gamepad1.right_stick_x < -.4) {
+                drive.turnLeftTank(1 * -gamepad1.right_stick_x);
             }
-        } else if(Math.abs(gamepad1.left_stick_x)>.4 || Math.abs(gamepad1.left_stick_y)>.4) { // If the left stick is being moved sufficiently
+        } else if (Math.abs(gamepad1.left_stick_x) > .4 || Math.abs(gamepad1.left_stick_y) > .4) { // If the left stick is being moved sufficiently
             // Forward/Back
             if (gamepad1.left_stick_y < -.4 && Math.abs(gamepad1.left_stick_x) < .4) {
-                drive.moveForward(1*-gamepad1.left_stick_y);
+                drive.moveForward(1 * -gamepad1.left_stick_y);
             }
             if (gamepad1.left_stick_y > .4 && Math.abs(gamepad1.left_stick_x) < .4) {
-                drive.moveBackward(1*gamepad1.left_stick_y);
+                drive.moveBackward(1 * gamepad1.left_stick_y);
             }
             // Left/Right
             if (gamepad1.left_stick_x < -.4 && Math.abs(gamepad1.left_stick_y) < .4) {
-                drive.moveRight(1*-gamepad1.left_stick_x);
+                drive.moveRight(1 * -gamepad1.left_stick_x);
             }
             if (gamepad1.left_stick_x > .4 && Math.abs(gamepad1.left_stick_y) < .4) {
-                drive.moveLeft(1*gamepad1.left_stick_x);
+                drive.moveLeft(1 * gamepad1.left_stick_x);
             }
             // Diagonals
             if (gamepad1.left_stick_y < -.4 && gamepad1.left_stick_x > .4) {
@@ -201,63 +219,88 @@ public class RevampDecodeDrive {
      * @param r Reverse Direction
      */
     public void runIntake(boolean run, boolean r){
-        if(run){
-            intake.setPower(intakeSpeed*reverse(r));
-        }else{
+        if(run) {
+            intake.setPower(intakeSpeed * reverse(r));
+            int drumPos = drumRotor.getTargetPosition();
+            if (drumPos % (ROTATION_TICK / 3) <= ROTATION_TICK / 7) {
+                drumRotor.setTargetPosition((int) (drumPos / (ROTATION_TICK / 3)) * (ROTATION_TICK / 3) + ROTATION_TICK / 6);
+            }
+            if(isGreenOrPurple()!=-1
+                    && drumRotor.getCurrentPosition() >= drumRotor.getTargetPosition()-ROTATION_TICK/16) {
+                drumRotor.setTargetPosition(drumPos+ROTATION_TICK/3);
+                // Shift over everything in ball storage
+                balls[2] = balls[1];
+                balls[1] = balls[0];
+                // Adds our new ball
+                balls[0] = isGreenOrPurple();
+            }
+        } else {
             intake.setPower(0);
         }
         telemetry.addData("Intake Speed", intakeSpeed);
     }
 
     /**
-     * Runs the outtake if the boolean is true
-     * @param run Whether or not the outtake should be active
-     * @param r Reverse Direction
+     * Handles all of the outtake
+     * @param prime Set to true once to make the spinners start
+     * @param cancel Set to true once to make the spinners stop
+     * @param firePurple Set to true once to fire a purple, then stop the spinners after it's launched
+     * @param fireGreen Set to true once to fire a green, then stop the spinners after it's launched
      */
-    public void runOuttake(boolean run, boolean r){
-        if(run) {
-            launcherRight.setPower(launchSpeed);
+    public void runOuttake(boolean prime, boolean cancel, boolean firePurple, boolean fireGreen){
+        if (prime) {
             launcherLeft.setPower(launchSpeed);
-        } else {
-            launcherRight.setPower(0);
+            launcherRight.setPower(launchSpeed);
+        }else {
             launcherLeft.setPower(0);
+            launcherRight.setPower(0);
         }
-        telemetry.addData("Launcher Speed", launchSpeed);
+        if (cancel) {
+            launcherLeft.setPower(0);
+            launcherRight.setPower(0);
+        }
+        if (firePurple) {
+            drumRotor.setTargetPosition(0);
+        }
+    }
+    /**
+     * Posts all ball positions to telemetry
+     */
+    public void ballTelemetry(){
+        for(int i=0; i<balls.length; i++){
+            if(balls[i] == 0){
+                telemetry.addData("Ball "+i, "Green");
+            }
+            else if(balls[i] == 1){
+                telemetry.addData("Ball "+i+" (Launch Position)", "Purple");
+            }
+            else{
+                telemetry.addData("Ball "+i, "N/A");
+            }
+        }
+    }
+    /**
+     * Finds the color, and reports it to telemetry
+     */
+    private double[] getColor() {
+        double red = BallColor.red();
+        double green = BallColor.green();
+        double blue = BallColor.blue();
+        return new double[]{red, green, blue};
     }
 
     /**
-     * Runs the drum based on the booleans
-     * @param runForward Run the drum in the forward direction
-     * @param runBackward Run the drum in the backward direction
+     *
+     * @return 0 if green, 1 if purple, -1 if neither
      */
-    public void runDrum(boolean runForward, boolean runBackward){
-        if (runForward){
-            drumRotor.setTargetPosition(drumRotor.getTargetPosition()+ROTATION_TICK/3);
+    private int isGreenOrPurple(){
+        double colors[] = getColor();
+        if(colors[1] > colors[2] * 1.5 && colors[1] > colors[0] * 1.5){
+            return 0;
         }
-        else if(runBackward){
-            drumRotor.setTargetPosition(drumRotor.getTargetPosition()-ROTATION_TICK/3);
+        if (colors[2] > colors[1] * 1.2 && colors[0] > colors[1] * 1.2){
+            return 1;
         }
-        telemetry.addData("Drum Target", drumRotor.getTargetPosition());
-    }
-
-    /**
-     * Make the kicker kick (position 1)
-     */
-    public void deployKicker(){
-        setKickerPosition(0.65);
-    }
-    /**
-     * Sets the kicker to a specific position
-     * @param position Position between 0 and 1
-     */
-    public void setKickerPosition(double position){
-        kicker.setPosition(position);
-    }
-
-    /**
-     * Make the kicker recall (position 0)
-     */
-    public void returnKicker(){
-        setKickerPosition(1);
+        return -1;
     }
 }

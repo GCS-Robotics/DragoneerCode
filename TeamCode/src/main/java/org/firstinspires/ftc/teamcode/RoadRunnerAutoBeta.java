d1f24380e4f469eab86f67eade4ccd482e0be6e1
david <davidc1@glenelg.org>
2025-11-17 16:59:10 -0500

    Created a rough outline for the path that the robot will take in auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoBeta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RoadRunnerAutoBeta.java
@@ -15,18 +15,83 @@ public class RoadRunnerAutoBeta extends LinearOpMode {
     public void runOpMode() throws InterruptedException {
         MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0, 0, 0));
 
+
         waitForStart();
 
+
         Actions.runBlocking(
-                drive.actionBuilder(new Pose2d(60, 60, 180))
+                drive.actionBuilder(new Pose2d(0,0,0))
                         .turnTo(-90)
                         .lineToX(12)
                         .lineToY(-24)
                         .build());
         Actions.runBlocking(
                 drive.actionBuilder(new Pose2d(12, -24, -90))
-                        .lineToY(-34)
-
+                        .lineToY(-34.5)
+                        .lineToY(-39.5)
+                        .lineToY(-44.5)
                         .build());
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(12, -44.5, -90))
+                        .lineToY(-24)
+                        .lineToX(24)
+                        .turnTo(45)
+                        .build());
+
+        // Launch Balls
+
+
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(24, -24, 45))
+                        .turnTo(-90)
+                        .lineToX(-12)
+                        .lineToY(-24)
+                        .build());
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(-12, -24, -90))
+                        .lineToY(-34.5)
+                        .lineToY(-39.5)
+                        .lineToY(-44.5)
+                        .build());
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(-12, -44.5, -90))
+                        .lineToY(-24)
+                        .lineToX(24)
+                        .turnTo(45)
+                        .build());
+        // Launch Balls
+
+
+
+
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(24, -24, 45))
+                        .turnTo(-90)
+                        .lineToX(-36)
+                        .lineToY(-24)
+                        .build());
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(-36, -24, -90))
+                        .lineToY(-34.5)
+                        .lineToY(-39.5)
+                        .lineToY(-44.5)
+                        .build());
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(-36, -44.5, -90))
+                        .lineToY(-24)
+                        .lineToX(24)
+                        .turnTo(45)
+                        .build());
+        // Launch Balls
+
+        //Go to home
+        Actions.runBlocking(
+                drive.actionBuilder(new Pose2d(24, -24, 45))
+                        .lineToY(36)
+                        .lineToX(-36)
+                        .turnTo(0)
+                        .build());
+
     }
 }

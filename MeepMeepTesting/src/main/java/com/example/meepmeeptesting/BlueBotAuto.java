fe8031dff066876c0972e22a33386e919147997a
Robert <robertakurc@gmail.com>
2025-11-28 19:23:46 -0500

    Updated Auto to be blue, and remove the unnecessary going to the base.

--- MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueBotAuto.java
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/BlueBotAuto.java
@@ -3,23 +3,24 @@ package com.example.meepmeeptesting;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.core.colorscheme.scheme.ColorSchemeBlueDark;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
 
-public class MeepMeepTesting {
+public class BlueBotAuto {
     public static void main(String[] args) {
-
+        System.setProperty("sun.java2d.opengl", "true");
         MeepMeep meepMeep = new MeepMeep(800);
 
         RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                .setColorScheme(new ColorSchemeBlueDark())
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
         myBot.runAction(
-                myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
+                myBot.getDrive().actionBuilder(new Pose2d(0, 0, Math.toRadians(90)))
 
                         // FIRST CYCLE
-                        .turnTo(Math.toRadians(90))
                         .strafeTo(new Vector2d(-12, -24))
                         .strafeTo(new Vector2d(-12, -34.5))
                         .strafeTo(new Vector2d(-12, -39.5))
@@ -28,7 +29,7 @@ public class MeepMeepTesting {
                         .strafeTo(new Vector2d(-24, -24))
                         .turnTo(Math.toRadians(225))
 
-// SECOND CYCLE
+                        // SECOND CYCLE
                         .turnTo(Math.toRadians(90))
                         .strafeTo(new Vector2d(12, -24))
                         .strafeTo(new Vector2d(12, -34.5))
@@ -38,7 +39,7 @@ public class MeepMeepTesting {
                         .strafeTo(new Vector2d(-24, -24))
                         .turnTo(Math.toRadians(225))
 
-// THIRD CYCLE
+                        // THIRD CYCLE
                         .turnTo(Math.toRadians(90))
                         .strafeTo(new Vector2d(36, -24))
                         .strafeTo(new Vector2d(36, -34.5))
@@ -47,13 +48,6 @@ public class MeepMeepTesting {
                         .strafeTo(new Vector2d(36, -24))
                         .strafeTo(new Vector2d(-24, -24))
                         .turnTo(Math.toRadians(225))
-
-// GO HOME
-                        .strafeTo(new Vector2d(-24, 36))
-                        .strafeTo(new Vector2d(38, 34))
-                        .turnTo(Math.toRadians(180))
-
-
                         .build()
         );
 

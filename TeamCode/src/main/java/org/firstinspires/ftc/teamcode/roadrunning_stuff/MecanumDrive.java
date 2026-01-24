6d86f8c1e79a127c3815c76c0183c6e76e72cfde
Robert <robertakurc@gmail.com>
2026-01-24 05:59:20 -0500

    Increased kA to try and reduce slippery-ness.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunning_stuff/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunning_stuff/MecanumDrive.java
@@ -70,7 +70,7 @@ public final class MecanumDrive {
         // feedforward parameters (in tick units)
         public double kS = 1.1839379074741165;
         public double kV = 0.0007027517353713855;
-        public double kA = 0.000015;
+        public double kA = 0.00015;
 
         // path profile parameters (in inches)
         public double maxWheelVel = 150;
